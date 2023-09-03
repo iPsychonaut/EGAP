009f9e3620ed57ea394cef9aea26006bfb2c8af1
Ian M. B <99409346+iPsychonaut@users.noreply.github.com>
2023-09-02 21:19:47 -0700

    Update EGAP_main.py
    Separated installation and setup of prerequisites into separate module.

--- EGAP_main.py
+++ EGAP_main.py
@@ -5,14 +5,22 @@ Created on Fri Jun 16 11:27:53 2023
 @author: ian.michael.bollinger@gmail.com with the help of ChatGPT 4.0
 
 Command Line Example:
-    python EGAP_main.py --input_dir /path/to/folder --organism_kingdom STRING --genome_size INTEGER --primer_type STRING --org_data [same/different] --resource_use INTEGER --attempt_install [0/1]
+    python EGAP_main.py -i /path/to/folder -k STRING -g INTEGER -p STRING -d [same/different] -r INTEGER -a [0/1]
 
-The --input_dir must have sub-folders with names containing either 'illumina' (sub-folder with raw PE150 .fq.gz files and their matching MD5.txt file) AND a folder with 'ont' (sub-folder with raw pass .fastq.gz files)
-The --organism_kingdom must be from the following: Archaea, Bacteria, Fauna, Flora, Funga, or Protista
-The --genome_size is a number in Mega-Bytes/Bases that the expected genome is to be. 
-The --primer_type must be a string similar to 'TruSeq3-PE' to represent the Illumina primer type to use with trimmomatic.
+The -i, --input_dir must have sub-folders with names containing either 'illumina' (sub-folder with raw PE150 .fq.gz files and their matching MD5.txt file) AND a folder with 'ont' (sub-folder with raw pass .fastq.gz files)
+The -k, --organism_kingdom must be from the following: Archaea, Bacteria, Fauna, Flora, Funga, or Protista
+The -g, --genome_size is a number in Mega-Bytes/Bases that the expected genome is to be. 
+The -p, --primer_type must be a string similar to 'TruSeq3-PE' to represent the Illumina primer type to use with trimmomatic.
 """
+# Base Python Imports
 import sys, subprocess, argparse, multiprocessing, math, os, platform, shutil
+from threading import Thread
+
+# Required Python Imports
+import psutil
+
+# Custom Python Imports
+from EGAP_setup import setup_module, install_module
 
 # Define and parse command line arguments
 parser = argparse.ArgumentParser(description="Run Entheome Illumina+ONT Pipeline")
@@ -24,26 +32,33 @@ default_genome_size = 60
 default_primer = 'TruSeq3-PE'
 default_organism_data = 'same' # same = Ian's same organism pipeline; different = Kelsey's Slot Lab different organism pipeline
 default_percent_resources = 80
-default_install = '0' # 0 = Attempt to install; 1 = Skip install
+default_install = '0' # 0 = Skip install; 1 = Attempt to install
 
 # Add organism data argument
-parser.add_argument('--input_dir', type=str, default=default_folder, # Ps_azurescens #Ps_caeruleorhiza # Ps_semilanceata # Ps_zapatecorum/PUBLICATION_REPLICATION
+parser.add_argument('--input_dir', '-i',
+                    type = str, default = default_folder,
                     help='The root folder containing a folder with "illumina" in its name and one with "ont" in its name')
-parser.add_argument('--organism_kingdom',default = default_organism_kingdom,
-                    choices=['Archaea', 'Bacteria', 'Fauna', 'Flora', 'Funga', 'Protista'],
+parser.add_argument('--organism_kingdom', '-k', 
+                    type = str, default = default_organism_kingdom,
+                    choices = ['Archaea', 'Bacteria', 'Fauna', 'Flora', 'Funga', 'Protista'],
                     help = f'Kingdom the current organism data belongs to. (default: {default_organism_kingdom})')
-parser.add_argument('--genome_size', type = int, default = default_genome_size,
+parser.add_argument('--genome_size', '-g',
+                    type = int, default = default_genome_size,
                     help = f'Genome Size. (default: {default_genome_size})')
-parser.add_argument('--primer_type', default = default_primer,
+parser.add_argument('--primer_type', '-p',
+                    type = str, default = default_primer,
                     help = f'Type of Illumina Primers used. (default: {default_primer})')
-parser.add_argument('--org_data', type=str, default=default_organism_data,
-                    choices=["same", "different"],
-                    help='Indicate if the provided data are generated from the same organism or different organisms')
-parser.add_argument('--resource_use', type = int, default = default_percent_resources,
+parser.add_argument('--organism_data', '-d',
+                    type = str, default = default_organism_data,
+                    choices = ["same", "different"],
+                    help = 'Indicate if the provided data are generated from the same organism or different organisms')
+parser.add_argument('--resource_use', '-r',
+                    type = int, default = default_percent_resources,
                     help = f'Percent of Resources to use. (default: {default_percent_resources})')
-parser.add_argument('--attempt_install', default=default_install,
-                    choices=['0', '1'], 
-                    help=f'Flag to indicate if the installation should be attempted 0 = No; 1 = Yes (default: {default_install}).')
+parser.add_argument('--attempt_install', '-a',
+                    type = str, default = default_install,
+                    choices = ['0', '1'], 
+                    help = f'Flag to indicate if the installation should be attempted 0 = No; 1 = Yes (default: {default_install}).')
 
 # Parse the arguments
 args = parser.parse_args()
@@ -51,94 +66,42 @@ BASE_FOLDER = args.input_dir
 CURRENT_ORGANISM_KINGDOM = args.organism_kingdom
 GENOME_SIZE = args.genome_size
 ILLU_PRIMER_TYPE = args.primer_type
-ORGANISM_DATA = args.org_data
+ORGANISM_DATA = args.organism_data
 PERCENT_RESOURCES = (args.resource_use/100)
 EGAP_ATTEMPT_INSTALL = args.attempt_install
 
-# Function to install a specific python module
-def install_module(module_name):
-    """
-    Installs a provided python module with mamba, accepts versions as well.
-
-    Args:
-    module_name (str): The name of the module that can be installed with mamba, versions are acceptable.
-    """
-    # Make sure module is installed
-    install_cmd = ['mamba', 'install', '-y', '-c', 'bioconda', '-c', 'agbiome', '-c', 'prkrekel', '-c', 'conda-forge', module_name]
-    print(f"UNLOGGED CMD:\t{' '.join(install_cmd)}")
-    intsall_result = subprocess.check_call(install_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if intsall_result == 0:
-        print(f'UNLOGGED PASS:\tSuccessfully installed: {module_name}')
-    else:
-        print(f'UNLOGGED ERROR:\tUnable to install: {module_name}')
-
 # Check if already tried installing required Python libraries
 if EGAP_ATTEMPT_INSTALL == '1':  
-    # Make sure mamba is installed
-    module_name = 'mamba==1.5.0'
-    print(f"UNLOGGED:\tAttempting to install: {module_name}...")
-    install_cmd = ['conda', 'install', '-y', '-c', 'conda-forge', module_name]
-    print(f"UNLOGGED CMD:\t{' '.join(install_cmd)}")
-    intsall_result = subprocess.check_call(install_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if intsall_result == 0:
-        print(f'UNLOGGED PASS:\tSuccessfully installed: {module_name}')
-    else:
-        print(f'UNLOGGED ERROR:\tUnable to install: {module_name}')
+    # Gather module information
+    module_name = 'EGAP'
+    python_libraries = ['requests==2.31.0', 'busco==5.5.0','openjdk==20.0.0', 'nanoq==0.10.0', 'pandas==2.0.3',
+                        'biopython==1.81', 'tqdm==4.38.0', 'termcolor==2.3.0', 'beautifulsoup4==4.12.2',
+                        'fastqc==0.11.8', 'quast==5.2.0', 'nanostat==1.6.0', 'flye==2.9.2',
+                        'bbtools==37.62', 'metaeuk==6.a5d39d9', 'blast==2.14.1', 'bwa==0.7.17',
+                        'minimap2==2.26', 'pysam==0.21.0', 'samtools==1.17', 'arcs==1.2.5',
+                        'tigmint==1.2.10', 'abyss==2.3.7', 'racon==1.5.0', 'spades==3.15.3',
+                        'psutil==5.9.5']
+    java_program_dict = {"trimmomatic": "http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip",
+                         "pilon": "https://github.com/broadinstitute/pilon/releases/download/v1.24/pilon-1.24.jar"}
+    prereq_list = ["java", "fastqc", "quast.py", "busco", "samtools", "bwa",
+                   "makeblastdb", "blastn", "racon", "nanoq", "NanoStat",
+                   "spades.py", "tblastn", "flye", "minimap2", "metaeuk", "tqdm"]
+    ### TODO: ADD KELSEY'S PREREQUISITE: abyss-sealer
     
-    # Ensure all other libraries are installed
-    libraries = ['requests==2.31.0', 'busco==5.5.0','openjdk==20.0.0', 'nanoq==0.10.0', 'pandas==2.0.3',
-                 'biopython==1.81', 'tqdm==4.38.0', 'termcolor==2.3.0', 'beautifulsoup4==4.12.2',
-                 'fastqc==0.11.8', 'quast==5.2.0', 'nanostat==1.6.0', 'flye==2.9.2',
-                 'bbtools==37.62', 'metaeuk==6.a5d39d9', 'blast==2.14.1', 'bwa==0.7.17',
-                 'minimap2==2.26', 'pysam==0.21.0', 'samtools==1.17', 'arcs==1.2.5',
-                 'tigmint==1.2.10', 'abyss==2.3.7', 'racon==1.5.0', 'spades==3.15.3',
-                 'gdown==4.7.1', 'psutil==5.9.5']
-    print(f'UNLOGGED:\tAttempting to install the following Python Libraries: {libraries}')
-    try:
-        for library in libraries:
-            # Run a subprocess calling conda install for each missing library
-            install_module(library)
-    except:
-        # Print an error if something goes wrong during the installation
-        print(f"UNLOGGED ERROR:\t Unable to Install {library}")      
-elif EGAP_ATTEMPT_INSTALL == '0':
-    print(f'UNLOGGED:\tSkipping Python Libraries installation')
-
-if EGAP_ATTEMPT_INSTALL == '1':  
-    import requests, zipfile, gdown
-
-    # File ID extracted from the Google Drive link for the Databases
-    file_id = '1i2zSQ4G0t9gWHL2ndIQHweXCkfwLX8N2'
-    output_path = 'file.zip'  # Output filename
-    unzip_path = '~/EGAP/'
-    url = f'https://drive.google.com/uc?id={file_id}'
-
-    # Check if the EGAP_Database directory already exists
-    if os.path.exists(output_path):
-        print(f"EGAP_Database directory already exists at {output_path}. Skipping download and extraction.")
-    else:
-        # Download Databases from Google Drive then Unzip the downloaded and Remove the zip file
-        print(f"UNLOGGED:\tAttempting to download EGAP_Databases.zip from Google Drive...")
+    # Run main setup for the module
+    setup_module(module_name, python_libraries, java_program_dict, prereq_list)
     
-        # Download Zip with gdown
-        gdown.download(url, output_path, quiet=False)
-        
-        # Unzip contents and then remove zip file
-        with zipfile.ZipFile(output_path, 'r') as zip_ref:
-            zip_ref.extractall(extract_to)        
-        os.remove(output_path)
-                    
-        # Set the flag to indicate we've attempted installation
-        os.environ['EGAP_ATTEMPT_INSTALL'] = '0'
-        
-        # Reset the script with loaded libraries
-        print('UNLOGGED PASS:\tRestarting EGAP')
-        os.execv(sys.executable, ['python'] + sys.argv + ['--attempt_install', '0'])
+    # Set the flag to indicate we've attempted installation
+    os.environ['EGAP_ATTEMPT_INSTALL'] = '0'
+    
+    # Reset the script with loaded libraries
+    print('UNLOGGED PASS:\tRestarting EGAP')
+    os.execv(sys.executable, ['python'] + sys.argv + ['--attempt_install', '0'])
 elif EGAP_ATTEMPT_INSTALL == '0':
-    print(f'UNLOGGED:\tSkipping Python Libraries installation')
-
-from threading import Thread
-from check_tools import get_env_dir, check_for_jars, check_prereqs_installed
+    print(f'UNLOGGED:\tSkipping Module setup and prerequisite installation')
+    
+# Custom Python Imports
+from check_tools import get_env_dir
 from log_print import log_print, generate_log_file
 from EGAP_ONT import process_ONT
 from EGAP_illumina import process_illumina
@@ -164,24 +127,17 @@ def PILON_POLISH_PIPELINE(BASE_FOLDER, CURRENT_ORGANISM_KINGDOM, GENOME_SIZE, IL
     """
     # Get the number of CPUs available on the system
     num_cpus = multiprocessing.cpu_count()
-    
-    try:
-        import psutil
-    except ModuleNotFoundError:
-        psutil_module = 'psutil==5.9.5'  
-        install_module(psutil_module)
-        import psutil
-    
+        
     # Get the amount of RAM (GB) currently available
     mem_info = psutil.virtual_memory()
     
     # Calculate the number of threads as based on PERCENT_RESOURCES of available CPUs & RAM
-    cpu_threads = int(math.floor(num_cpus * PERCENT_RESOURCES))
-    ont_cpus = int(math.floor(cpu_threads/2))
+    cpu_threads = round(int(num_cpus * PERCENT_RESOURCES),0)
+    ont_cpus = round(int(cpu_threads/2),0)
     illu_cpus = cpu_threads - ont_cpus
 
     ram_gb = int(mem_info.total / (1024.0 ** 3) * PERCENT_RESOURCES)
-    ont_ram_gb = int(math.floor(ram_gb/2))
+    ont_ram_gb = round(int(ram_gb/2),2)
     illu_ram_gb = ram_gb - ont_cpus
     
     ont_percent_resources = float(round(PERCENT_RESOURCES/2, 1))
@@ -190,7 +146,7 @@ def PILON_POLISH_PIPELINE(BASE_FOLDER, CURRENT_ORGANISM_KINGDOM, GENOME_SIZE, IL
     # Set the boolean checks for each folder to False
     processed_same_illumina = False
     processed_same_ont = False
-    
+
     # Walk through the root folder
     for folder_name, subfolders, file_names in os.walk(BASE_FOLDER):
         if folder_name == BASE_FOLDER:
@@ -409,37 +365,21 @@ if __name__ == "__main__":
     debug_log = f'{BASE_FOLDER}EGAP_log.tsv'
     log_file = generate_log_file(debug_log, use_numerical_suffix=False)
     log_print('RUNNING ENTHEOME GENOME EXTRACTION PIPELINE - EGAP', log_file)
-
-    # Check for specific Third-Party JAR Files with adjusted program_dict
-    program_dict = {"trimmomatic": ("https://github.com/usadellab/Trimmomatic",
-                                "trimmomatic-*.jar",
-                                "http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip"),
-                    "pilon": ("https://github.com/broadinstitute/pilon",
-                              "pilon-*.jar",
-                              "https://github.com/broadinstitute/pilon/releases/download/v1.24/pilon-1.24.jar")}
-    jar_paths_dict, _ = check_for_jars(program_dict, 'EGAP')
-
-    # Check Pipeline Third-Party Prerequisites
-    prerequisites = ["java", "fastqc", "quast.py", "busco", "samtools", "bwa",
-                     "makeblastdb", "blastn", "racon", "nanoq", "NanoStat",
-                     "spades.py", "tblastn", "flye", "minimap2", "metaeuk", "tqdm"]
-    check_prereqs_installed(prerequisites)
-    ### TODO: ADD KELSEY'S PREREQUISITE: abyss-sealer
     
     # Generate BUSCO Database Dictionary
-    busco_db_dict = {'Archaea':  [f'{environment_dir}/EGAP/BUSCO_Databases/archaea_odb10',
-                                  f'{environment_dir}/EGAP/BUSCO_Databases/euryarchaeota_odb10',],
-                     'Bacteria': [f'{environment_dir}/EGAP/BUSCO_Databases/actinobacteria_phylum_odb10',
-                                  f'{environment_dir}/EGAP/BUSCO_Databases/proteobacteria_odb10',],
-                     'Fauna':    [f'{environment_dir}/EGAP/BUSCO_Databases/vertebrata_odb10',
-                                  f'{environment_dir}/EGAP/BUSCO_Databases/arthropoda_odb10',],
-                     'Flora':    [f'{environment_dir}/EGAP/BUSCO_Databases/eudicots_odb10',
-                                  f'{environment_dir}/EGAP/BUSCO_Databases/liliopsida_odb10'],
-                     'Funga':    [f'{environment_dir}/EGAP/BUSCO_Databases/basidiomycota_odb10',
-                                  f'{environment_dir}/EGAP/BUSCO_Databases/agaricales_odb10'],
-                     'Protista': [f'{environment_dir}/EGAP/BUSCO_Databases/alveolata_odb10',
-                                  f'{environment_dir}/EGAP/BUSCO_Databases/euglenozoa_odb10']}
-
+    busco_db_dict = {'Archaea':  [f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/archaea_odb10',
+                                  f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/euryarchaeota_odb10',],
+                     'Bacteria': [f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/actinobacteria_phylum_odb10',
+                                  f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/proteobacteria_odb10',],
+                     'Fauna':    [f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/vertebrata_odb10',
+                                  f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/arthropoda_odb10',],
+                     'Flora':    [f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/eudicots_odb10',
+                                  f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/liliopsida_odb10'],
+                     'Funga':    [f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/basidiomycota_odb10',
+                                  f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/agaricales_odb10'],
+                     'Protista': [f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/alveolata_odb10',
+                                  f'{environment_dir}/EGAP/EGAP_Databases/BUSCO_Databases/euglenozoa_odb10']}
+   
     # Modify your pipeline function to accept the new argument and adjust its behavior accordingly
     if ORGANISM_DATA == 'same':
         log_print("RUN IAN'S SAME ORGANISM DATA PIPELINE", log_file)
