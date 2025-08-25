b977692266c43184dcf4c85302f4217328a43d3b
Ian M. B. <99409346+iPsychonaut@users.noreply.github.com>
2025-08-24 20:35:02 -0600

    Update build.sh v3.2
    Sanity version update

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
+
 # 1) Install the Python package files (your bin/ folder) into site-packages
 mkdir -p "${SP_DIR}"
 cp -r bin "${SP_DIR}/bin"
@@ -8,4 +9,5 @@ cp -r bin "${SP_DIR}/bin"
 # 2) Install the EGAP entry-point
 mkdir -p "${PREFIX}/bin"
 cp EGAP.py "${PREFIX}/bin/EGAP"
+
 chmod +x "${PREFIX}/bin/EGAP"
